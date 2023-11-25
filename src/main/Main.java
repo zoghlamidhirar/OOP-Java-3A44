2530e4997577bacb0070c826911815f4a07f7574
zoghlamidhirar <dhirar.zoghlami@esprit.tn>
2023-11-25 23:51:51 +0100

    Prosit 10: HashSet et TreeSet

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,48 @@
 package main;
 
-import entities.Employe;
-import entities.SocieteArrayList;
+import entities.*;
+import java.util.*;
+
 
 
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
+
+        Departement dep1 = new Departement(1, "IT", 50);
+        Departement dep2 = new Departement(2, "HR", 30);
+        Departement dep3 = new Departement(3, "Finance", 40);
+
+        DepartementHashSet departementHashSet = new DepartementHashSet();
+
+        departementHashSet.ajouterDepartement(dep1);
+        departementHashSet.ajouterDepartement(dep2);
+        departementHashSet.ajouterDepartement(dep3);
+
+        System.out.println("Liste des départements :");
+        departementHashSet.displayDepartement();
+
+        System.out.println("Recherche du département 'HR' : " + departementHashSet.rechercherDepartement("HR"));
+
+        // Test rechercherDepartement avec un objet
+        System.out.println("Recherche du département 'dep1' : " + departementHashSet.rechercherDepartement(dep1));
+
+
+        System.out.println("Liste des départements triés par ID :");
+        TreeSet<Departement> sortedDepartements = departementHashSet.trierDepartementById();
+        for (Departement d : sortedDepartements) {
+            System.out.println(d);
+        }
+
+        departementHashSet.supprimerDepartement(dep2);
+
+        System.out.println("Liste des départements après suppression du département 'HR' :");
+        departementHashSet.displayDepartement();
+
+
+
+//THe code down belooow isss to test in the main for the previous Prosit
+        /*
 
         SocieteArrayList societe = new SocieteArrayList();
 
@@ -55,6 +91,8 @@ public class Main {
         System.out.println("List of employees after sorting by name, department, and grade:");
         societe.displayEmploye();
 
+
+*/
 
 
 
